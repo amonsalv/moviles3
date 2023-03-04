b6983233ee697cb2ebcf5a4c541059479e5d3ddd
anamonsalve <anamonsalve96@gmail.com>
2023-03-04 10:23:43 -0500

    creation another app in react

--- practica1/array.js
+++ practica1/array.js
@@ -2,16 +2,16 @@
 
 //Generar el arreglo de objetos
 let inmuebles = [
-    {idimbueble: '01', direccion: 'calle1', telefono:'312312', precio: 332, estado: true},
-    {idimbueble: '02', direccion: 'calle2', telefono:'354335', precio: 243, estado: true},
-    {idimbueble: '03', direccion: 'calle3', telefono:'315333', precio: 503, estado: false},
-    {idimbueble: '04', direccion: 'calle4', telefono:'353242', precio: 345, estado: true},
-    {idimbueble: '05', direccion: 'calle5', telefono:'334542', precio: 300, estado: false},
+    { idimbueble: '01', direccion: 'calle1', telefono: '312312', precio: 332, estado: true },
+    { idimbueble: '02', direccion: 'calle2', telefono: '354335', precio: 243, estado: true },
+    { idimbueble: '03', direccion: 'calle3', telefono: '315333', precio: 503, estado: false },
+    { idimbueble: '04', direccion: 'calle4', telefono: '353242', precio: 345, estado: true },
+    { idimbueble: '05', direccion: 'calle5', telefono: '334542', precio: 300, estado: false },
 ]
 
 //referenciar elementos del html
-let idimbueble = document.getElementById('idinmueble');
-let mIdInmueble = document.querySelector('#mIdInmueble');
+let idimbueble = document.getElementById('idimueble');
+let mIdImueble = document.querySelector('#mIdImueble');
 
 let direccion = document.getElementById('direccion');
 let mdireccion = document.querySelector('#mdireccion');
@@ -31,31 +31,92 @@ let listar = document.getElementById('listar');
 
 //Eventos
 
- // evento mostrar mensajes 
- idinmueble.addEventListener('focus',function(){
-    midinmueble.textContent= "Id. inmueble es obligatorio"
+// evento mostrar mensajes 
+idimbueble.addEventListener('focus', function () {
+    mIdImueble.textContent = "Id. inmueble es obligatorio"
 })
-idinmueble.addEventListener('blur',function(){
-    midinmueble.textContent= ""
-})
-
-direccion.addEventListener('focus',function(){
-    mdireccion.textContent= "la direccion es obligatorio"
-})
-direccion.addEventListener('blur',function(){
-    mdireccion.textContent= ""
+mIdImueble.addEventListener('blur', function () {
+    mIdImueble.textContent = ""
 })
 
-telefono.addEventListener('focus',function(){
-    mtelefono.textContent= "El telefono es obligatorio"
+direccion.addEventListener('focus', function () {
+    mdireccion.textContent = "la direccion es obligatorio"
 })
-telefono.addEventListener('blur',function(){
-    mtelefono.textContent= ""
+direccion.addEventListener('blur', function () {
+    mdireccion.textContent = ""
 })
 
-precio.addEventListener('focus',function(){
-    mprecio.textContent= "El precio es obligatorio"
+telefono.addEventListener('focus', function () {
+    mtelefono.textContent = "El telefono es obligatorio"
 })
-precio.addEventListener('blur',function(){
-    mprecio.textContent= ""
+telefono.addEventListener('blur', function () {
+    mtelefono.textContent = ""
 })
+
+precio.addEventListener('focus', function () {
+    mprecio.textContent = "El precio es obligatorio"
+})
+precio.addEventListener('blur', function () {
+    mprecio.textContent = ""
+})
+
+//Enviar
+enviar.addEventListener('click', () => {
+
+    //buscar idimueble
+    let encuentra = inmuebles.find(item => item.idimbueble == idimbueble.value);
+    if (encuentra==undefined){ //no lo encuentra
+        //Agregar el objeto en el arreglo de objetos (inmuebles)
+    idimbueble.push({
+        idimbueble: idimbueble.value,
+        direccion: direccion.value,
+        telefono: telefono.value,
+        precio: precio.value,
+        estado: disponible, checked
+    })
+
+    console.log(inmuebles);
+    
+
+    }
+
+    
+});
+
+
+function agregarInmueble() {
+    // Leer los valores ingresados por el usuario
+    let idinmuebleValor = idimbueble.value;
+    let direccionValor = direccion.value;
+    let telefonoValor = telefono.value;
+    let precioValor = precio.value;
+    let estadoValor = disponible.checked;
+
+    // Validar que se haya ingresado la información requerida
+    if (!idinmuebleValor || !direccionValor || !telefonoValor || !precioValor) {
+        alert('Debe ingresar la información requerida');
+        return;
+    }
+
+    // Crear un nuevo objeto inmueble con los valores ingresados por el usuario
+    let nuevoInmueble = {
+        idinmueble: idinmuebleValor,
+        direccion: direccionValor,
+        telefono: telefonoValor,
+        precio: precioValor,
+        estado: estadoValor
+    };
+
+    // Agregar el nuevo objeto inmueble al arreglo de inmuebles
+    inmuebles.push(nuevoInmueble);
+
+    // Limpiar los campos del formulario
+    idinmueble.value = '';
+    direccion.value = '';
+    telefono.value = '';
+    precio.value = '';
+    disponible.checked = false;
+
+    // Notificar al usuario que se ha agregado el nuevo inmueble
+    alert('Inmueble agregado correctamente');
+}
